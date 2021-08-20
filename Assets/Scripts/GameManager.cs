73ba46dc29b5784106a79a8881c2704547226a2b
cshoaib05 <choudharyshoaib05@gmail.com>
2021-08-20 14:47:13 +0530

    added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,15 +9,14 @@ public class GameManager : MonoBehaviour
     public GameObject Line;
     public GameObject FillPrefab;
     public GameObject Marker;
-   
+ 
     public static Color selectedColor = Color.black;
     public static string mode;
     public static int trimCount = 0;
-    public static int sortLayerCount = 4;
+    public static int sortLayerCount = 3;
     int fillcount = 0;
     public List<GameObject> ColorList;
     Vector3 camPos;
-
     public ParticleSystem[] confetti;
 
     private void Start()
@@ -34,31 +33,22 @@ public class GameManager : MonoBehaviour
                 CreateFill();
                 fillcount++;
             }
-
             if (Input.GetMouseButton(0) && !IsPointerOverUIObject())
             {
-
-                Marker.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(20f, 20f, 1f));
+                Marker.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, -70f, 3f));
                 Fill.transform.localScale = Fill.transform.localScale * 1.05f;
             }
         }
-
     }
-
 
     void CreateFill()
     {
         Fill = Instantiate(FillPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, 0f, 3f)), Quaternion.identity);
         Fill.GetComponent<SpriteRenderer>().color = selectedColor;
-        if (sortLayerCount != 4)
-        {
-            Fill.GetComponent<SpriteRenderer>().sortingOrder = sortLayerCount;
-        }
+        Fill.GetComponent<SpriteRenderer>().sortingOrder = sortLayerCount;
         ColorList.Add(Fill);
         sortLayerCount--;
     }
-
-
 
     private bool IsPointerOverUIObject()
     {
@@ -68,5 +58,4 @@ public class GameManager : MonoBehaviour
         EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
         return results.Count > 0;
     }
-
 }
